f3225298384305f49b4979e19e6e5c3b7fd6a1a1
sermawutor <leslieashigbi@gmail.com>
2023-05-07 03:59:32 -0700

    task

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
